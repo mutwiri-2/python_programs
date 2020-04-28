5cb2aa77e534a7e7fe7a93c8dd4458b45e9608fe
mutwiri-2 <devmutwiri2@gmail.com>
2020-04-28 12:34:49 +0300

    add definition and the language rules

---
+++ pig_latin/pig_latin.py
@@ -0,0 +1,45 @@
+# Pig Latin is a language game or argot in which words in English are altered, usually by adding a fabricated suffix or by moving the onset or initial consonant or consonant cluster of a word to the end of the word and adding a vocalic syllable to create such a suffix
+
+# Rules:
+# 1. For words that begin with consonant sounds, all letters before the initial vowel are placed at the end of the word sequence. Then, "ay" is added for example:
+# "pig" = "igpay"
+# "latin" = "atinlay"
+# "banana" = "ananabay"
+# "will" = "illway"
+# "butler" = "utlerbay"
+# "happy" = "appyhay"
+# "duck" = "uckday"
+# "me" = "emay"
+
+# 2. For words that begin with vowel sounds, the vowel is left alone, and most commonly 'yay' is added to the end. But in different parts of the world, there are different 'dialects' of sorts. Some people may add 'way' or just 'ay' or other endings. Examples are:
+
+# "eat" = "eatyay" or "eatay"
+# "omelet" = "omeletyay" or "omeletay"
+# "are" = "areyay" or "areay"
+# "egg" = "eggyay" or "eggay"
+# "explain" = "explainyay"
+# "always" = "alwaysyay" or "alwaysay"
+# "ends" = "endsyay" or "endsay"
+# "honest" = "honestyay"
+# "I"= "Iyay"
+
+# 3. When words begin with consonant clusters (multiple consonants that form one sound), the whole sound is added to the end when speaking or writing.
+
+# "smile" = "ilesmay"
+# "string" = "ingstray"
+# "stupid" = "upidstay"
+# "glove" = "oveglay"
+# "trash" = "ashtray"
+# "floor"= "oorflay"
+# "store"= "orestay"
+
+# 4. An alternative convention for words beginning with vowel sounds, one removes the initial vowel(s) along with the first consonant or consonant cluster. This usually only works for words with more than one syllable and offers a variant of the words in keeping with the mysterious, unrecognizable sounds of the converted words. Examples are:
+
+# "every" = "eryevay"
+# "another" = "otheranay"
+# "under" = "erunday"
+# "island" = "andislay"
+# "elegant" = "egantelay"
+
+# for more, read >>> https://en.wikipedia.org/wiki/Pig_Latin
+
