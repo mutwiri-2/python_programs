71a91c453411e586f0ddb6c5b0069f5c6c1944ba
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-01 18:52:34 +0300

    a shorter alternate implementation

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -5,7 +5,25 @@ upper_bound = 20
 secret_number = random.randint(lower_bound, upper_bound)
 chances = 5
 
+# print("I am thinking of a number between {} and {}...".format(lower_bound, upper_bound))
+# for number_of_guess in range(1, chances+1):
+#     print("Number of guesses remaining: {}".format(chances+1 - number_of_guess))
+#     guess = int(input("Type your guess and press enter: "))
+#     if guess > secret_number and number_of_guess != chances:
+#         print("Try a lower value")
+#     elif guess < secret_number and number_of_guess != chances:
+#         print("Try a higher value")
+#     else:
+#         break
+
+# if guess == secret_number:
+#     print("You win!! You got it right in {} guesses".format(number_of_guess))
+# else:
+#     print("You loose!! My number was {}".format(secret_number))
+
+# alternate solution
 print("I am thinking of a number between {} and {}...".format(lower_bound, upper_bound))
+
 for number_of_guess in range(1, chances+1):
     print("Number of guesses remaining: {}".format(chances+1 - number_of_guess))
     guess = int(input("Type your guess and press enter: "))
@@ -13,10 +31,7 @@ for number_of_guess in range(1, chances+1):
         print("Try a lower value")
     elif guess < secret_number and number_of_guess != chances:
         print("Try a higher value")
-    else:
-        break
+    elif guess == secret_number:
+        print("You win!! You got it right in {} guesses".format(number_of_guess))
 
-if guess == secret_number:
-    print("You win!! You got it right in {} guesses".format(number_of_guess))
-else:
-    print("You loose!! My number was {}".format(secret_number))
+print("You loose!! My number was {}".format(secret_number))
