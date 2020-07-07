61b52cc8b23238d75251f45f5b5fb319bf3f31ee
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-07 12:03:59 +0300

    add wikipedia-style bullet points

--- wikipedia_bullet_point_adder/bullet_point_adder.py
+++ wikipedia_bullet_point_adder/bullet_point_adder.py
@@ -1,5 +1,6 @@
 #! python3
-# bullet_point_adder.py - inserts wikipedia bullet points (*) to the start of each line of text on the clipboard
+# bullet_point_adder.py - inserts wikipedia bullet points (*) to the start of
+# each line of text on the clipboard
 
 import pyperclip
 
@@ -7,7 +8,7 @@ text = pyperclip.paste() # get text from clipboard
 
 lines = text.split('\n')  # get a list of all lines in the text copied
 for i in range(len(lines)):
-    lines[i] = '* ' + lines[i] # add bullet point to each line
-text = '\n'.join(lines)  # join the modified lines into a string ready to be copied to the clipboard and reassign the text variable to the new string
+    lines[i] = '* ' + lines[i] # add bullet point and space to beginning of each line
+text = '\n'.join(lines)  # join the modified lines into a string
 
 pyperclip.copy(text)
